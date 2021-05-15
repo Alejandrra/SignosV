f7a2b6cd4fc9021d01f62d3430a95b47ae98775f
Alejandrra <nicole73711@gmail.com>
2021-05-15 18:44:32 -0500

    Subir

---
+++ app/src/main/java/com/example/signosvitales/Nivel_Oxigeno.java
@@ -0,0 +1,14 @@
+package com.example.signosvitales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nivel_Oxigeno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nivel__oxigeno);
+    }
+}
---
+++ app/src/main/java/com/example/signosvitales/Ritmo_Cardiaco.java
@@ -0,0 +1,14 @@
+package com.example.signosvitales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ritmo_Cardiaco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ritmo__cardiaco);
+    }
+}
